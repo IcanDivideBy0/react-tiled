0d00e0d0fbce8e61b916b8e7c90712c0b60ba5fb
Samuel Hurel <samuel.hurel@gmail.com>
2018-07-31 10:36:41 +0200

    Update demo

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import { MapLoader, Map } from "react-tiled";
+import styled from "styled-components";
 
-import "./App.css";
-
-const MAP_URL = process.env.PUBLIC_URL + "/assets/map.json";
+const AppWrapper = styled.div`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #1c1117;
+`;
 
 class App extends Component {
   render() {
     return (
-      <MapLoader mapUrl={MAP_URL}>
-        <Map />
+      <MapLoader mapUrl={process.env.PUBLIC_URL + "/assets/map.json"}>
+        <AppWrapper>
+          <Map style={{ transform: "scale(2)" }} />
+        </AppWrapper>
       </MapLoader>
     );
   }
